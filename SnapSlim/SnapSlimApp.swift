48b56e03d173d5d59ec3d59c0f795d2ff75bb426
方君宇 <fangjunyu.com@gmail.com>
2025-07-19 14:58:17 +0800

    创建Xcode项目文件

---
+++ SnapSlim/SnapSlimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSlimApp.swift
+//  SnapSlim
+//
+//  Created by 方君宇 on 2025/7/19.
+//
+
+import SwiftUI
+
+@main
+struct SnapSlimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
